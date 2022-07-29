9216e1d7357ca45eba172b9d00e2eb164a6414c5
Nicolas Lavoura <nicolas.lavoura@gmail.com>
2022-07-29 18:17:46 -0300

    Layout do componente Popover (Pequeno container para notificar que um item foi adicionado ao carrinho) finalizado

---
+++ src/components/Popover/popover.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './popover.css';
+
+export function Popover() {
+  return (
+    <div className='popover'>
+      <div class='popover__arrow'></div>
+      <div className='popover__title'>
+        <label>Adicionado com sucesso</label>
+      </div>
+      <div className='popover__content'>
+        <div className='popover__content-title'>
+          <label>Oferta Cheddar Bacon</label>
+        </div>
+        <div className='popover__content-description'>
+          <label>Ingredientes:</label>
+          <ul className='popover__content-description__list'>
+            <li>1 Carne 250gr</li>
+            <li>2 queijo Cheddar</li>
+            <li>1 Bacon</li>
+            <li>Molho especial</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
