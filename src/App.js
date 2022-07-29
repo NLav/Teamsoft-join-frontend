86c0fffbf2bcbc02d05e944e9e67f4276c5ef03b
Nicolas Lavoura <nicolas.lavoura@gmail.com>
2022-07-29 15:41:50 -0300

    Layout da pagina do produto para Desktop finalizado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Navbar } from './components/Navbar/navbar';
+import { ProductInformation } from './components/ProductInformation/productInformation';
+import { ProductExtras } from './components/ProductExtras/productExtras';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='app'>
+      <Navbar />
+      <div className='app__content'>
+        <ProductInformation />
+        <ProductExtras />
+      </div>
     </div>
   );
 }
