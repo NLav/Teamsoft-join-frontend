37f5438ea712a5b645bb18f30fa7b77cf86945a2
Nicolas Lavoura <nicolas.lavoura@gmail.com>
2022-07-29 15:36:50 -0300

    Layout do componente Navbar (Barra de navegacao do topo da tela) finalizado

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { AddressSelector } from '../AddressSelector/addressSelector';
+import './navbar.css';
+
+export function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='navbar__logo'>
+        <img className='navbar__logo-img' src='/Images/logo.png' alt='deliverize logo' />
+      </div>
+      <div className='navbar__address'>
+        <AddressSelector />
+      </div>
+      <div className='navbar__search'>
+        <input
+          className='navbar__search-input'
+          placeholder='Busque por estabelecimento ou produtos'
+        />
+      </div>
+      <div className='navbar__item'>
+        <button className='navbar__item-link'>
+          <img className='navbar__item-link__img' src='/Images/ic_login.png' alt='entrar icone' />
+          Entrar
+        </button>
+        <button className='navbar__item-link'>
+          <img className='navbar__item-link__img' src='/Images/ic_cart.png' alt='carrinho icone' />
+          Carrinho
+        </button>
+      </div>
+    </div>
+  );
+}
