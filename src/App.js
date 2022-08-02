ca424118ef73928180df152f02940d802a719bbb
Nicolas Lavoura <nicolas.lavoura@gmail.com>
2022-08-02 18:18:23 -0300

    Logica adicionada ao App, consulta API e envia para onde necessario

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import './App.css';
 import { Navbar } from './components/Navbar/navbar';
 import { ProductInformation } from './components/ProductInformation/productInformation';
 import { ProductExtras } from './components/ProductExtras/productExtras';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import config from './config/api.json';
 
 function App() {
+  const [apiInformation, setApiInformation] = useState([]);
+
+  useEffect(() => {
+    axios.get(config.api.url).then((res) => {
+      setApiInformation(res.data[0]);
+    });
+  }, []);
+
   return (
     <div className='app'>
       <Navbar />
       <div className='app__content'>
-        <ProductInformation />
+        <ProductInformation apiInformation={apiInformation} />
         <ProductExtras />
       </div>
     </div>
