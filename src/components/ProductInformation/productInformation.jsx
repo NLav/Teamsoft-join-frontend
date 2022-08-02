e72328a7232545221b255a3fe20f9b39c9443284
Nicolas Lavoura <nicolas.lavoura@gmail.com>
2022-08-02 18:15:31 -0300

    Logica adicionada ao componente ProductInformation, recebimento de informacoes via Props (vindo da API)

--- src/components/ProductInformation/productInformation.jsx
+++ src/components/ProductInformation/productInformation.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import './productInformation.css';
 
-export function ProductInformation() {
-  return (
+export function ProductInformation(props) {
+  return props.apiInformation ? (
     <div className='product'>
       <img className='product__picture' src='/Images/product.png' alt='product' />
-      <label className='product__title'>Oferta Picanha Cheddar Bacon</label>
-      <label className='product__description'>
-        Habúrguer de picanha, molho de picanha, cebola crispy, bacon, queijo cheddar, molho cheddar
-        e pão mix de gergelim
-      </label>
+      <label className='product__title'>{props.apiInformation.nm_product}</label>
+      <label className='product__description'>{props.apiInformation.description}</label>
       <div className='product__prices'>
-        <label className='product__prices-value'>R$31,99</label>
-        <label className='product__prices-value--old'>R$34,95</label>
+        <label className='product__prices-value'>R${props.apiInformation.vl_price}</label>
+        {props.apiInformation.vl_discount === 0 ? null : (
+          <label className='product__prices-value--old'>R${props.apiInformation.vl_discount}</label>
+        )}
       </div>
     </div>
-  );
+  ) : null;
 }
