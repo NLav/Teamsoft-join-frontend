b1c09b149259e68dddd649c7472ec249520a4c62
Nicolas Lavoura <nicolas.lavoura@gmail.com>
2022-08-02 18:11:18 -0300

    Logica adicionada ao componente Popover, animacao criada para mostrar/esconder o componente

--- src/components/Popover/popover.jsx
+++ src/components/Popover/popover.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import './popover.css';
 
-export function Popover() {
+export function Popover(props) {
   return (
-    <div className='popover'>
-      <div class='popover__arrow'></div>
-      <div className='popover__title'>
-        <label>Adicionado com sucesso</label>
-      </div>
-      <div className='popover__content'>
-        <div className='popover__content-title'>
-          <label>Oferta Cheddar Bacon</label>
+    <>
+      <div
+        className={props.togglePopover ? 'popover__arrow popover__arrow--hidden' : 'popover__arrow'}
+      ></div>
+      <div className={props.togglePopover ? 'popover popover--hidden' : 'popover'}>
+        <div className='popover__title'>
+          <label>Adicionado com sucesso</label>
         </div>
-        <div className='popover__content-description'>
-          <label>Ingredientes:</label>
-          <ul className='popover__content-description__list'>
-            <li>1 Carne 250gr</li>
-            <li>2 queijo Cheddar</li>
-            <li>1 Bacon</li>
-            <li>Molho especial</li>
-          </ul>
+        <div className='popover__content'>
+          <div className='popover__content-title'>
+            <label>Oferta Cheddar Bacon</label>
+          </div>
+          <div className='popover__content-description'>
+            <label>Ingredientes:</label>
+            <ul className='popover__content-description__list'>
+              <li>1 Carne 250gr</li>
+              <li>2 queijo Cheddar</li>
+              <li>1 Bacon</li>
+              <li>Molho especial</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
