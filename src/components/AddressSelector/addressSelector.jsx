df88ab4da885a3170581afefdec9fde2024bf08f
Nicolas Lavoura <nicolas.lavoura@gmail.com>
2022-08-02 18:10:05 -0300

    Logica adicionada ao componente AddressSelector, dropdown criado

--- src/components/AddressSelector/addressSelector.jsx
+++ src/components/AddressSelector/addressSelector.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './addressSelector.css';
 
-export function AddressSelector() {
+export function AddressSelector(props) {
+  // Current/Selected user address
+  const [userAddress, setUserAddress] = useState(props.addresses[0]);
+  // Show/Hide dropdown to select user address
+  const [toggleAddressesDropdown, setToggleAddressesDropdown] = useState(false);
+
   return (
-    <div className='address-selector'>
+    <div
+      className='address-selector'
+      onClick={() => {
+        toggleAddressesDropdown
+          ? setToggleAddressesDropdown(false)
+          : setToggleAddressesDropdown(true);
+      }}
+    >
       <div className='address-selector__text'>
         <label className='address-selector__text-title'>Entrega:</label>
-        <label className='address-selector__text-value'>R. Antonio Braune, 222</label>
+        <label className='address-selector__text-value'>{userAddress}</label>
       </div>
       <div className='address-selector__icon'>
         <img
@@ -15,6 +27,21 @@ export function AddressSelector() {
           alt='dropdown icon'
         />
       </div>
+      {/* Show dropdown if ToggleAddressesDropdown is true  */}
+      {toggleAddressesDropdown ? (
+        <div className='address-selector__dropdown'>
+          {props.addresses.map((address) => {
+            return (
+              <label
+                className='address-selector__dropdown-value'
+                onClick={() => setUserAddress(address)}
+              >
+                {address}
+              </label>
+            );
+          })}
+        </div>
+      ) : null}
     </div>
   );
 }
